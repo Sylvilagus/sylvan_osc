732ff42d1b7edf9ef0c8fa2a204af126c19d83a4
sylvilagus <122039238@qq.com>
2019-04-26 15:02:03 +0800

    delete some param

--- lib/constant/http_constant.dart
+++ lib/constant/http_constant.dart
@@ -2,9 +2,9 @@
  * Created by Sylva Lee
  */
 
-const APP_ID = "xR7U0hhw4ku8WGoCaPUH";
-const PRIVATE_KEY = "EJizvwRtXwbfaZlkZ6H3cxm8wsbysJk6";
-const REDIRECT_URL = "https%3A%2F%2Fwww.jianshu.com%2Fu%2F4db4d69b3b1d";
+const APP_ID = "你的id";
+const PRIVATE_KEY = "你的key";
+const REDIRECT_URL = "你的回调url";
 const HOST = "https://www.oschina.net";
 const OAUTH_URL = "$HOST/action/oauth2/authorize?client_id=$APP_ID&response_type=code&redirect_uri=$REDIRECT_URL" ;
 const TOKEN_URL = "$HOST/action/openapi/token";
